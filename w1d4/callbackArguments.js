eeb066cf3ceb40ab3e6166a408f73d48774d4ab7
MattEmond <memond36@gmail.com>
2017-11-02 15:24:25 +0000

    First commit

---
+++ w1d4/callbackArguments.js
@@ -0,0 +1,15 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found();   // execute callback
+    }
+  }
+}
+
+function actionWhenFound (arr) {
+
+  console.log("Found Waldo at: " );
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
